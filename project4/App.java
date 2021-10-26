1794f03042db216cb648c39f1e7cd88756b2acb1
rdavid01 <55700557+rdavid01@users.noreply.github.com>
2021-10-25 21:16:28 -0400

    files for project4

--- project4/App.java
+++ project4/App.java
@@ -6,6 +6,6 @@ package cs1501_p4;
 
 public class App {
     public static void main(String[] args) {
-        NetAnalysis na = new NetAnalysis("build/resources/main/network_data2.txt");
+        NetAnalysis na = new NetAnalysis("network_data2.txt");
     }
 }
